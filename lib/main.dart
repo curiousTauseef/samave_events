b94a056c4952061a9fbf6d90adfee8f84317b0e3
ktejas <krishnasalimeda@gmail.com>
2018-09-24 15:20:50 +0530

    Imported to Android Studio

--- lib/main.dart
+++ lib/main.dart
@@ -64,6 +64,7 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
+    Re
     return new Scaffold(
       appBar: new AppBar(
         // Here we take the value from the MyHomePage object that was created by
